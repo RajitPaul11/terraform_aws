a17e4f71953d7dc85e82239d0235304f26a95cb3
Rajit Paul <rajitpaull@gmail.com>
2022-02-10 15:10:05 +0530

    Resource Attribute & Depends on

---
+++ resource_atr_and_dependencies/main.tf
@@ -0,0 +1,11 @@
+resource "aws_key_pair" "alpha" {
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+  key_name = "beta"
+}
+
+resource "aws_instance" "gama" {
+  ami = "ami-0c6615d1e95c98aca"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.alpha.key_name
+  depends_on = [aws_key_pair.alpha]
+}
